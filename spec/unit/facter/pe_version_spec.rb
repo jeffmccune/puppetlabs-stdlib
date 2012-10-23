579d084d508cdba1aaa5def2ed25630035e5f7bb
Matthaus Owens <matthaus@puppetlabs.com>
2012-10-23 14:14:06 -0700

    Add spec tests for pe_version facts
    This commit adds some basic spec tests for the pe_version facts. There are
    basic postitive and negative cases.

---
+++ spec/unit/facter/pe_version_spec.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe "PE Version specs" do
+  context "If PE is installed" do
+    %w{ 2.6.1 2.10.300 }.each do |version|
+      puppetversion = "2.7.19 (Puppet Enterprise #{version})"
+      context "puppetversion => #{puppetversion}" do
+        before :all do
+          Facter.fact(:puppetversion).stubs(:value).returns(puppetversion)
+        end
+
+        after :all do
+          Facter.clear
+        end
+
+        (major,minor,patch) = version.split(".")
+
+        it "Should return true" do
+          Facter.fact(:is_pe).value.should == true
+        end
+
+        it "Should have a version of #{version}" do
+          Facter.fact(:pe_version).value.should == version
+        end
+
+        it "Should have a major version of #{major}" do
+          Facter.fact(:pe_major_version).value.should == major
+        end
+
+        it "Should have a minor version of #{minor}" do
+          Facter.fact(:pe_minor_version).value.should == minor
+        end
+
+        it "Should have a patch version of #{patch}" do
+          Facter.fact(:pe_patch_version).value.should == patch
+        end
+      end
+    end
+  end
+
+  context "If PE is not installed" do
+    before :all do
+      Facter.fact(:puppetversion).stubs(:value).returns("2.7.19")
+    end
+
+    after :all do
+      Facter.clear
+    end
+
+    it "is_pe is false" do
+      Facter.fact(:is_pe).value.should == false
+    end
+
+    it "pe_version is nil" do
+      Facter.fact(:pe_version).value.should be_nil
+    end
+
+    it "pe_major_version is nil" do
+      Facter.fact(:pe_major_version).value.should be_nil
+    end
+
+    it "pe_minor_version is nil" do
+      Facter.fact(:pe_minor_version).value.should be_nil
+    end
+
+    it "Should have a patch version" do
+      Facter.fact(:pe_patch_version).value.should be_nil
+    end
+  end
+end
